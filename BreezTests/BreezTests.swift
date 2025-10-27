03f84564528cffc3529cc870bf837bb204674edc
0OverRated0 <adhulife@gmail.com>
2025-10-27 10:08:56 -0700

    Initial Commit

---
+++ BreezTests/BreezTests.swift
@@ -0,0 +1,17 @@
+//
+//  BreezTests.swift
+//  BreezTests
+//
+//  Created by Advaith Akella on 10/27/25.
+//
+
+import Testing
+@testable import Breez
+
+struct BreezTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
